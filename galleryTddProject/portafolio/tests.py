a9568040c9465512dd0b1d1f19fe0e57bad773bd
SebastianMujica22 <42355577+SebastianMujica22@users.noreply.github.com>
2020-03-25 08:26:12 -0500

    ROJO-PASO1

---
+++ galleryTddProject/portafolio/tests.py
@@ -0,0 +1,17 @@
+
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
+# Create your tests here.
+from .models import Portafolio
+import json
+
+# Create your tests here.
+class PortafolioTestCase(TestCase):
+
+    def test_list_portafolios_status(self):
+        url = 'portafolio'
+        response = self.client.get(url, Format = 'json')
+        self.assertEqual(response.status_code, 200)
+
+
