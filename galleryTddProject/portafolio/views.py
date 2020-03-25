fb1d7122e53aa1ff2d21ad73daef1b92e2b4d693
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-25 10:04:04 -0500

    VERDE-PASO2

--- galleryTddProject/portafolio/views.py
+++ galleryTddProject/portafolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from .models import Portafolio
@@ -8,4 +8,5 @@ import json
 # Create your views here.
 @csrf_exempt
 def index(request):
-    return HttpResponse(serializers.serialize("json", []))
+    portafolios_list = Portafolio.objects.all()
+    return HttpResponse(serializers.serialize("json", portafolios_list))
